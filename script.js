c4edc3603e2e34b399363d3eb690fa92892444ff
jedimanela <85641505+jedimanela@users.noreply.github.com>
2026-03-16 14:00:19 -0700

    Adding header to all pages
    Added new css header that will work on all pages. Added button functionality to the header.

---
+++ script.js
@@ -0,0 +1,11 @@
+function bestPicturePressed() {
+    window.location.href = "bestPicture.html";
+}
+
+function redCarpetPressed() {
+    window.location.href = "bestLooks.html";
+}
+
+function winnersPressed() {
+    window.location.href = "winners.html";
+}
