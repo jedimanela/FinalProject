25f2edda26909d7d93dea3043d30fb70df2597d9
jedimanela <85641505+jedimanela@users.noreply.github.com>
2026-03-18 13:26:04 -0700

    styled index
    Finished added to landing page

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
 function bestPicturePressed() {
-    window.location.href = "bestPicture.html";
+    createStars();
+
+   document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+        window.location.href = "bestPicture.html";
+    }, 500);
 }
 
 function redCarpetPressed() {
-    window.location.href = "bestLooks.html";
+    createStars();
+
+    document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+        window.location.href = "bestLooks.html";
+    }, 500);
 }
 
 function winnersPressed() {
-    window.location.href = "winners.html";
+    createStars();
+    
+    document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+        window.location.href = "winners.html";
+    }, 500);
+}
+
+function createStars() {
+    const container = document.getElementById("star-container");
+
+    for (let i = 0; i < 20; i++) {
+        let star = document.createElement("div");
+        star.classList.add("star");
+        star.innerHTML = "★";
+
+        // random position
+        star.style.left = Math.random() * window.innerWidth + "px";
+        star.style.top = Math.random() * window.innerHeight + "px";
+
+        // random size
+        star.style.fontSize = (Math.random() * 20 + 10) + "px";
+
+        container.appendChild(star);
+
+        // remove after animation
+        setTimeout(() => {
+            star.remove();
+        }, 1000);
+    }
 }
